df7a982ff780f070c9016f73358d32953bb3cbaf
JS Chen <jschen@example.com>
2017-06-21 16:22:20 +0000

    changed calculate distance function

--- pointfinder-child-theme/includes/location_check.php
+++ pointfinder-child-theme/includes/location_check.php
@@ -51,22 +51,14 @@ if (!class_exists('DistanceCheck')) {
 
         }
 
-        function Calculate($dblLat1, $dblLong1, $dblLat2, $dblLong2) {
-            $EARTH_RADIUS_MILES = 3963;
-            $dist = 0;
-            //convert degrees to radians
-            $dblLat1 = $dblLat1 * M_PI / 180;
-            $dblLong1 = $dblLong1 * M_PI / 180;
-            $dblLat2 = $dblLat2 * M_PI / 180;
-            $dblLong2 = $dblLong2 * M_PI / 180;
-            if ($dblLat1 != $dblLat2 || $dblLong1 != $dblLong2) {
-                //the two points are not the same
-                $dist = sin($dblLat1) * sin($dblLat2) + cos($dblLat1) * cos($dblLat2) * cos($dblLong2 - $dblLong1);
-                $dist = $EARTH_RADIUS_MILES * (-1 * atan($dist / sqrt(1 - $dist * $dist)) + M_PI / 2);
-            }
-            return $dist;
-        }
-
-    }
+        function Calculate($lat1, $lon1, $lat2, $lon2) {
+					$theta = $lon1 - $lon2;
+				  $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
+				  $dist = acos($dist);
+				  $dist = rad2deg($dist);
+				  $miles = $dist * 60 * 1.1515;
+					return $miles;
+				}
+  }
 
 }
